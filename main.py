4489cc2913cee0b48eb8cb170861382536bfba69
Davit Bakhshyan <a1l2o3@bk.ru>
2026-05-20 06:26:07 +0400

    interactive and custom draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.widgets import Button
 from rrt.rrt_plus import RRT
 
 
@@ -89,6 +90,137 @@ def run_static_3d():
     plt.show()
 
 
+def run_interactive_2d():
+    print("\n--- Running Interactive 2D RRT ---")
+    bounds = [[0, 20], [0, 20]]
+    obstacles = [(5, 5, 2), (10, 10, 3), (15, 5, 2), (5, 15, 2), (14, 15, 2.5)]
+
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.set_xlim(bounds[0]); ax.set_ylim(bounds[1])
+    ax.set_title("CLICK 2 POINTS: Start (Blue), then Goal (Red)")
+
+    for (ox, oy, orad) in obstacles:
+        ax.add_artist(plt.Circle((ox, oy), orad, color='gray', alpha=0.5))
+    plt.draw()
+
+    points = plt.ginput(2, timeout=-1)
+    if len(points) < 2: return
+
+    start = list(points[0])
+    goal = list(points[1])
+    ax.plot(start[0], start[1], "xb", markersize=10, label="Start")
+    ax.plot(goal[0], goal[1], "xr", markersize=10, label="Goal")
+    ax.set_title("Calculating...")
+
+    plt.ion()
+    def draw_step(from_node, to_node):
+        ax.plot([from_node.coords[0], to_node.coords[0]], 
+                [from_node.coords[1], to_node.coords[1]], "-g", alpha=0.4)
+        plt.pause(0.001)
+
+    rrt = RRT(start, goal, bounds, obstacles, step_size=1.0, max_iter=1000)
+    path = rrt.plan(draw_callback=draw_step)
+
+    if path:
+        path = np.array(path)
+        ax.plot(path[:, 0], path[:, 1], "-r", linewidth=2, label="Path")
+        ax.legend()
+    plt.ioff()
+    plt.show()
+
+
+class ObstacleDrawer:
+    def __init__(self, ax):
+        self.ax = ax
+        self.drawing = False
+        self.drawn_points = []
+        self.c1 = self.ax.figure.canvas.mpl_connect('button_press_event', self.on_press)
+        self.c2 = self.ax.figure.canvas.mpl_connect('button_release_event', self.on_release)
+        self.c3 = self.ax.figure.canvas.mpl_connect('motion_notify_event', self.on_motion)
+
+    def on_press(self, event):
+        if event.inaxes == self.ax and event.button == 1:
+            self.drawing = True
+            self.add_point(event.xdata, event.ydata)
+
+    def on_release(self, event):
+        if event.button == 1: self.drawing = False
+
+    def on_motion(self, event):
+        if self.drawing and event.inaxes == self.ax:
+            self.add_point(event.xdata, event.ydata)
+
+    def add_point(self, x, y):
+        self.drawn_points.append(np.array([x, y]))
+        self.ax.add_artist(plt.Circle((x, y), 0.5, color='gray'))
+        self.ax.figure.canvas.draw_idle()
+        
+    def disconnect(self):
+        self.ax.figure.canvas.mpl_disconnect(self.c1)
+        self.ax.figure.canvas.mpl_disconnect(self.c2)
+        self.ax.figure.canvas.mpl_disconnect(self.c3)
+
+def run_custom_draw_2d():
+    print("\n--- Running Custom Draw 2D RRT ---")
+    bounds = [[0, 20], [0, 20]]
+    start = [2, 2]; goal = [18, 18]
+
+    fig, ax = plt.subplots(figsize=(7, 7))
+    plt.subplots_adjust(bottom=0.2)
+    ax.set_xlim(bounds[0]); ax.set_ylim(bounds[1])
+    ax.set_title("Draw obstacles (Left Click). Click RUN when done.")
+
+    ax.plot(start[0], start[1], "xb", markersize=10, label="Start")
+    ax.plot(goal[0], goal[1], "xr", markersize=10, label="Goal")
+    
+    drawer = ObstacleDrawer(ax)
+    ax_btn = plt.axes([0.4, 0.05, 0.2, 0.075])
+    btn = Button(ax_btn, 'RUN RRT')
+
+    def run_algorithm(event):
+        drawer.disconnect()
+        btn.active = False
+        ax.set_title("Calculating Path...")
+        plt.ion()
+        
+        def draw_step(from_node, to_node):
+            ax.plot([from_node.coords[0], to_node.coords[0]], 
+                    [from_node.coords[1], to_node.coords[1]], "-g", alpha=0.4)
+            plt.pause(0.001)
+
+        rrt = RRT(start, goal, bounds, drawer.drawn_points, step_size=0.8, obs_type='drawn', clearance=0.6)
+        path = rrt.plan(draw_callback=draw_step)
+
+        if path:
+            path = np.array(path)
+            ax.plot(path[:, 0], path[:, 1], "-r", linewidth=2, label="Path")
+            ax.legend()
+        plt.ioff()
+        plt.draw()
+
+    btn.on_clicked(run_algorithm)
+    plt.show()
+
+
 if __name__ == '__main__':
-    #run_static_2d()
-    run_static_3d()
+    while True:
+        print("\n==================================")
+        print(" RRT / RRT* ALGORITHM COLLECTION")
+        print("==================================")
+        print("1. Static 2D Example (RRT*)")
+        print("2. Static 3D Example (Fast Rendering)")
+        print("3. Interactive 2D (Click Start/Goal)")
+        print("4. Custom Draw 2D (Draw Obstacles)")
+        print("5. Exit")
+        
+        choice = input("Enter choice (1-5): ")
+        
+        if choice == '1': run_static_2d()
+        elif choice == '2': run_static_3d()
+        elif choice == '3': run_interactive_2d()
+        elif choice == '4': run_custom_draw_2d()
+        elif choice == '5': 
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice, please try again.")
